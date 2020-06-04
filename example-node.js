446a5b4087c7ddcb5f680a686c794152b84e7cd4
Zixiao Sun <200400996@student.georgianc.on.ca>
2020-06-04 11:20:56 -0400

    finished my first express app

---
+++ example-node.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+// we need to require a library to use to do API request
+const https = require("https");
+
+
+
+
+app.get("/", (request, response) => {
+    // this is Controller
+    https.get('https://insult.mattbas.org/api/insult', resp => {
+        resp.on('data', chunk => {
+            response.send(`
+                <http>
+                    <head><title>Oh Snap</title></head>
+
+                    <body>
+                        <h1>Oh Snap!</h1>
+                        <p style = "font-size: 24px; color: blue;">
+                            ${chunk.toString()}!
+                        </p>
+                </http>
+            `)
+        });
+    })
+});
+
+app.listen(3000);
+
+
